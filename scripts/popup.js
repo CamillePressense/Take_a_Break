cc9957313ebe26fb82d36f28a24d76faaed18880
leoV0 <leo.cigale@gmail.com>
2025-04-24 14:13:35 +0200

    Ajout de l'event sur le bouton commencer

---
+++ scripts/popup.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const getButton = document.querySelector("#start");
+  getButton.addEventListener("click", () => {
+    window.close();
+  });
+});
