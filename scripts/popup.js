a461964a09093049f944fa73709ea37bc93ea99c
Mlherriteau <michel.lheriteau@hotmail.fr>
2025-04-24 14:36:50 +0200

    foncionnalité bouton plus ou moins temps de pause

---
+++ scripts/popup.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const MINUS_WORK_BUTTON = document.querySelector("#workMin");
+    const WORK_TIME = document.querySelector("#workTime");
+
+    MINUS_WORK_BUTTON.addEventListener("click", () => {
+        let workTime = Number(WORK_TIME.value);
+        workTime -= 1;
+        WORK_TIME.value = workTime;
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    const PLUS_WORK_BUTTON = document.querySelector("#workPlus");
+    const WORK_TIME = document.querySelector("#workTime");
+    PLUS_WORK_BUTTON.addEventListener("click", () => {
+        let workTime = Number(WORK_TIME.value);
+        workTime += 1;
+        WORK_TIME.value = workTime;
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    const MINUS_BREAK_BUTTON = document.querySelector("#breakMin");
+    const PLUS_BREAK_BUTTON = document.querySelector("#breakPlus");
+    const BREAK_TIME_INPUT = document.querySelector("#workTimeBreak");
+          
+
+    MINUS_BREAK_BUTTON.addEventListener("click", function () {
+        let breakTime = Number(BREAK_TIME_INPUT.value);
+        if (breakTime > 1) {
+            breakTime -= 1;
+            BREAK_TIME_INPUT.value = breakTime;
+        }
+    });
+          
+    PLUS_BREAK_BUTTON.addEventListener("click", () => {
+        let breakTime = Number(BREAK_TIME_INPUT.value);
+        breakTime += 1;
+        BREAK_TIME_INPUT.value = breakTime;
+    });
+});
+          
